5856605b1323c0bf8b64faf49e5b3d6845a6691c
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-01 10:12:00 +0600

    array and types

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -1,3 +1,51 @@
 // basic data types
 
+// string
+// explisive 
+let firstName = 'tuhin'
+
+let middleName: string = 'mahmud'
+
+// emplesive
+let lastName: number = 20;
+
+// boolean 
+let isAdmin: boolean = true;
+//undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+
+
+// ------------
+let d: number;
+
+d = 1234;
+
+let e: string;
+
+e = 'tuhin'
+
+let f: boolean;
+f = false;
+
+let g: null;
+g = null;
+
+
+
+// array
+// infra
+// defined explesible
+let friends: string[] = ['tuhin', 'mahmudul']
+// friends.push(36)
+let eligibleRollList: number[] = [30, 20]
+eligibleRollList.push(34)
+// eligibleRollList.push('hasan')
+let othersFriends: string[] = ['mahmudul', 'hasan', 'tuhin']
+
+othersFriends.push('parvej')
+
 
